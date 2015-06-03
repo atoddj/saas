326c6d1fab7da4b3a25faf9fb56b1de2b483e9a5
Todd Jones <atoddj@gmail.com>
2015-06-03 16:16:34 +0000

    Added plan model, user model, and made the databases relate with plan_id column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603152643) do
+ActiveRecord::Schema.define(version: 20150603161336) do
 
   create_table "contacts", force: true do |t|
     t.string "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150603152643) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "plan_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
