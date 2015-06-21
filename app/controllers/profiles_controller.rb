698e52bd102fdb47d5e128d6aea05d21fc180aed
Todd Jones <atoddj@gmail.com>
2015-06-21 14:48:16 +0000

    Created form and edited profile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,6 +9,12 @@ class ProfilesController < ApplicationController
     #save the user profile to the database
     @user = User.find( params[:user_id] )
     @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path(params[:user_id])
+    else
+      render action: :new
+    end
   end
   
   private
